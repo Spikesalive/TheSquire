95137716968fc519cdde2d40e75b042a20a4caa2
Spikesalive <48239825+Spikesalive@users.noreply.github.com>
2020-02-09 13:15:32 +0800

    base game

---
+++ TheSquireGame/Assets/ButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+    public class ButtonScript : MonoBehaviour 
+{
+    public UnityEvent buttonDown;
+    public UnityEvent buttonUp;
+    public Sprite ButtonDownSprite;
+    public Sprite ButtonUpSprite;
+
+    void Awake()
+    {
+        if(buttonDown == null)
+        {
+            buttonDown = new UnityEvent();
+        }
+        if (buttonUp == null)
+        {
+            buttonUp = new UnityEvent();
+        }
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("down");
+        buttonDown.Invoke();
+        GetComponent<SpriteRenderer>().sprite = ButtonDownSprite;
+    }
+    void OnMouseUp()
+    {
+        Debug.Log("up");
+        buttonUp.Invoke();
+        GetComponent<SpriteRenderer>().sprite = ButtonUpSprite;
+    }
+}
---
+++ TheSquireGame/Assets/PlayerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float MovementSpeed;
+
+    public GameObject Player;
+    bool Left;
+    bool right;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Left == true)
+        {
+            Player.transform.Translate(Vector2.left * MovementSpeed * Time.deltaTime);
+        }
+        if (right == true)
+        {
+            Player.transform.Translate(Vector2.right * MovementSpeed * Time.deltaTime);
+        }
+    }
+
+    public void MoveRight()
+    {
+        right = true; 
+    }
+
+    public void MoveLeft()
+    {
+        Left = true;
+    }
+
+    public void StopRight()
+    {
+        right = false;
+    }
+
+    public void StopLeft()
+    {
+        Left = false;
+    }
+}
